be70875edb3490bcbd6314a2677000c63ce4a153
Kevin Frans <kevinfrans2@gmail.com>
2022-11-23 14:55:49 -0500

    pck setup

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     license='MIT',
     zip_safe=False,
     packages=['powderworld'],
+    include_package_data=True,
     install_requires=['torch',
                       'numpy',
                       'stable_baselines3',
